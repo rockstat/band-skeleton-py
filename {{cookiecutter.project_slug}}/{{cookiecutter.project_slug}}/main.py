2821b245a5e449fd536baec633f7bc1a2b8c99d1
Dmitry Rodin <madiedinro@gmail.com>
2018-10-26 20:56:00 +0300

    initial commit

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
@@ -0,0 +1,26 @@
+# Band service skeleton
+# (c) Dmitry Rodin 2018
+# ---------------------
+import asyncio
+from itertools import count
+from prodict import Prodict as pdict
+from band import expose, cleanup, worker, settings, logger, response
+
+state=pdict()
+
+@expose.handler()
+async def main(**params):
+    pass
+
+@worker()
+async def service_worker():
+    for num in count():
+        try:
+            if num == 0:
+                pass
+        except asyncio.CancelledError:
+            break
+        except Exception:
+            logger.exception('exc')
+        await asyncio.sleep(30)
+
